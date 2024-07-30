b536f5b02674f8dca39f7e9fbe177a59eb90f5e8
Wesley T. Benette <weluonf@gmail.com>
2024-07-30 20:43:27 -0300

    :sparkles: feat: adds HeaderComponent

---
+++ src/app/views/components/header/header.component.spec.ts
@@ -0,0 +1,9 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
+
+describe('HeaderComponent', () =>
+{
+
+});
